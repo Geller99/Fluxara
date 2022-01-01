6cd5e96c8f34244047a4dbb06a5c102e325ecffd
geller99 <gellera800@gmail.com>
2022-01-01 12:48:43 +0100

    edited response headers

--- Server/index.js
+++ Server/index.js
@@ -8,6 +8,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://fluxserver-fbqvi1vr1-geller99.vercel.app"); 
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  next();
+});
+
+
 
 routes.forEach((route) => {
   app[route.method](route.path, route.handler);
